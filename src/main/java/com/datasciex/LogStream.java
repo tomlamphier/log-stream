8bc371e07c0a92a3588b030ecae1c9d53d4a6163
tomlamphier <thomaslamphier@gmail.com>
2018-03-16 13:55:46 -0400

    Update LogStream.java

--- src/main/java/com/datasciex/LogStream.java
+++ src/main/java/com/datasciex/LogStream.java
@@ -1,125 +1,94 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+// Program LogStream - read streaming log data and detect brute force 
+//                     hacking attempts.
+//
+// Outline of Processing
+// ---------------------
+//   1. Get cmd line parameters.  These point to the Flume input stream source.
+//   2. Create a Redis client. This will be used to store any malicious IP
+//      addresses.  Other applications, notably an nginx server, can use 
+//      this to filter requests.
+//   3. Set up an input stream that points to the Flume source, convert that
+//      to a String format, then set up a sliding window on the stream.  We
+//      want to input new data at 1-minute intervals and process with 5 
+//      minutes of data since we are looking for a large number of POST's
+//      over a short period.
+//   4. Set up a processing loop for the windowed data:
+//        a.  Translate data from String to Java class.
+//        b.  Use Java class RDD to create a dataset.
+//        c.  Query dataset for repeated POST attempts from a given IP.
+//        d.  Store malicious IP's in Redis db.
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 package com.datasciex;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.streaming.*;
-import org.apache.spark.streaming.api.java.*;
-import org.apache.spark.streaming.flume.FlumeUtils;
-import org.apache.spark.streaming.flume.SparkFlumeEvent;
-import org.apache.flume.source.avro.AvroFlumeEvent;
+import org.apache.spark.rdd.RDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.function.ForeachFunction;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.Duration;
+import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.flume.FlumeUtils;
+import org.apache.spark.streaming.flume.SparkFlumeEvent;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.storage.StorageLevel;
 import java.util.List;
-
-import redis.clients.jedis.Jedis; 
+import redis.clients.jedis.Jedis;
 
 public final class LogStream {
   private LogStream() {
   }
 
+  private final static String FILEPATH = "access.log";
   public static void main(String[] args) throws Exception {
-    if (args.length != 3) {
-      System.err.println("Usage: LogStream <host> <port> <path-to-jar");
+    if (args.length != 2) {
+      System.out.println("Usage: LogStream <hostname> <port>");
       System.exit(1);
     }
 
-
-    Jedis jedis = new Jedis("localhost"); 
-    System.out.println("Connection to server sucessfully"); 
-    //check whether server is running or not 
-    System.out.println("Server is running: "+jedis.ping()); 
-
-    jedis.set("tutorial-name", "Redis tutorial");
-    System.out.println("Stored string in Redis: " + jedis.get("tutorial-name"));
-
     String host = args[0];
     int port = Integer.parseInt(args[1]);
-    String pathToJar = args[2];
 
-    Duration batchInterval = new Duration(60000);
-    SparkConf sparkConf = new SparkConf().setAppName("LogStream")
-        .setMaster("local[2]");
-    JavaStreamingContext ssc = new JavaStreamingContext(sparkConf, batchInterval);
+    Jedis jedis = new Jedis("localhost");
+    JavaSparkContext sc = new JavaSparkContext();
+    JavaStreamingContext strCxt = new JavaStreamingContext(sc, new Duration(60 * 1000));
     JavaReceiverInputDStream<SparkFlumeEvent> flumeStream =
-      FlumeUtils.createStream(ssc, host, port);
-
-    JavaDStream<SparkFlumeEvent> windowFlume = flumeStream.window(new Duration(300000), new Duration(60000));
-    JavaDStream<String> windowLines = windowFlume.map(e ->  new String(e.event().getBody().array()));
-
- // Convert RDDs of the words DStream to DataFrame and run SQL query
-    windowLines.foreachRDD((rdd, time) -> {
-    SparkSession spark = JavaSparkSessionSingleton.getInstance(rdd.context().getConf(), pathToJar);
-      // Convert JavaRDD[String] to JavaRDD[bean class] to DataFrame
-      JavaRDD<JavaRecord> rowRDD = rdd.map(line -> {
-        JavaRecord record = new JavaRecord();
-        record.init(line);
-        return record;
-      });
-      Dataset<Row> linesDataFrame = spark.createDataFrame(rowRDD, JavaRecord.class);
-
-      // Creates a temporary view using the DataFrame
-      linesDataFrame.createOrReplaceTempView("lines");
-
-      // Do word count on table using SQL and print it
-      Dataset<Row> lineCountsDataFrame =
-          spark.sql("select ip, requestUri, count(*) as ctr from lines group by ip, requestUri having count(*) > 5");
-      System.out.println("A========= " + time + "=========A");
-      lineCountsDataFrame.show();
-     
-      List<Row> rows = lineCountsDataFrame.collectAsList();
-      System.out.println("list length = " + rows.size());
-      System.out.println("B========= " + time + "=========B");
-
-      for (Row r: rows) {
-          //System.out.println("row contents: " + r.getString(0));
-          String ip_key = r.getString(0);
-          if (!jedis.exists(ip_key)) {
-              jedis.set(ip_key, "T");
-              jedis.expire(ip_key, 120);
+      FlumeUtils.createStream(strCxt, host, port, StorageLevel.MEMORY_ONLY());
+    JavaDStream<String> logLines = flumeStream.map(e -> new String(e.event().getBody().array()));
+    JavaDStream<String> logWindow = logLines.window(new Duration(5 * 60 * 1000),
+      new Duration(60 * 1000));
+    logWindow.foreachRDD((rdd, time) -> {
+      if (rdd.count() > 0) {
+        JavaRDD<JavaRecord> logRecs = rdd.map(new Function<String, JavaRecord>() {
+          public JavaRecord call(String line) {
+          JavaRecord record = new JavaRecord();
+          record.init(line);
+          return record;
           }
+        });
+        SparkSession ss = SparkSession.builder().getOrCreate();
+        Dataset<Row> logDF = ss.createDataFrame(logRecs, JavaRecord.class);
+        logDF.createOrReplaceTempView("logrecs");
+
+        Dataset<Row> ipCounts = ss.sql("select ip, requestUri, count(*) as ctr "
+          + "from logrecs where requestUri like 'POST%' group by ip, requestUri having count(*) > 5");
+        System.out.println("ipCounts count = " + ipCounts.count());
+        List<Row> rows = ipCounts.collectAsList();
+        for (Row r: rows) {
+          String ipKey = r.getString(0);
+          if (!jedis.exists(ipKey)) {
+            jedis.set(ipKey, "T");
+          }
+        }
       }
- 
-      System.out.flush();
     });
 
-
-    ssc.start();
-    ssc.awaitTermination();
+    strCxt.start();
+    strCxt.awaitTermination();
+    sc.stop();
   }
 }
-
-/** Lazily instantiated singleton instance of SparkSession */
-class JavaSparkSessionSingleton {
-  private static transient SparkSession instance = null;
-  public static SparkSession getInstance(SparkConf sparkConf, String pathToJar) {
-    if (instance == null) {
-      instance = SparkSession
-        .builder()
-        //.config(sparkConf)
-        .appName("LogStream")
-        .config("spark.jars", pathToJar)
-        .getOrCreate();
-    }
-    return instance;
-  }
-}
-
